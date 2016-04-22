d3501f3677bec6d02a67bd9763c6ebef00fc4fb1
diegozgz92 <diegosc_zgz92@hotmail.com>
2016-04-22 11:53:33 +0200

    routes

---
+++ routes/gestionVisitantes.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var router = express.Router();
+var mongoOp = require("../models/mongoVisitantes");
+
+router.get('/', function(req, res) {
+    var response = {};
+    mongoOp.find({},function(err,data){
+// Mongo command to fetch all data from collection.
+        if(err) {
+            response = {"error" : true,"message" : "Error fetching data"};
+        } else {
+            response = {"error" : false,"message" : data};
+        }
+        res.json(response);
+    });
+});
+
+/**
+ * Función que permite añade un dato de un visitante
+ */
+router.post('/', function(req,res) {
+    var db = new mongoOp();
+    var response = {};
+
+    db.user = req.body.user;
+
+    db.save(function (err) {
+        if (err) {
+            response = {"error": true, "message": "Error adding data"};
+        } else {
+            response = {"error": false, "message": "Data added"};
+        }
+        res.json(response);
+    });
+});
+
+/**
+ * Función que devuelve el usuario correspondiente al 'id'
+ */
+router.get('/:id', function(req,res){
+    var response = {};
+    mongoOp.findById( req.params.id ,function(err,data){
+        if(err) {
+            response = {"error" : true,"message" : "Error fetching data"};
+        } else {
+            response = {"error" : false,"message" : data};
+        }
+        res.json(response);
+    });
+});
+
+/**
+ * Función que permite modificar el usuario correspondiente al 'id'.
+ */
+router.put('/:id', function(req,res){
+    var response = {};
+
+    mongoOp.findById(req.params.id,function(err,data){
+        if(err) {
+            response = {"error" : true,"message" : "Error fetching data"};
+        } else {
+            if(req.body.listaFavoritos !== undefined) {
+                data.listaFavoritos = req.body.listaFavoritos;
+            }
+            if(req.body.listaSeguidores !== undefined) {
+                data.listaSeguidores = req.body.listaSeguidores;
+            }
+            data.save(function(err){
+                if(err) {
+                    response = {"error" : true,"message" : "Error updating data"};
+                } else {
+                    response = {"error" : false,"message" : "Data is updated for "+req.params.id};
+                }
+                res.json(response);
+            })
+        }
+    });
+})
+
+/**
+ * Función que permite eliminar el usuario que corresponde con el 'id'
+ */
+router.delete('/:id', function(req,res){
+    var response = {};
+
+    mongoOp.findById(req.params.id,function(err,data){
+        if(err) {
+            response = {"error" : true,"message" : "Error fetching data"};
+        } else {
+            mongoOp.remove({_id : req.params.id},function(err){
+                if(err) {
+                    response = {"error" : true,"message" : "Error deleting data"};
+                } else {
+                    response = {"error" : false,"message" : "Data associated with "+req.params.id+"is deleted"};
+                }
+                res.json(response);
+            });
+        }
+    });
+})
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,126 @@
 var express = require('express');
 var router = express.Router();
+var mongoOp = require("../models/mongoUser");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/**
+ * Funcion que devuelve los usuarios
+ */
+router.get('/', function(req, res) {
+  var response = {};
+  mongoOp.find({},function(err,data){
+// Mongo command to fetch all data from collection.
+    if(err) {
+      response = {"error" : true,"message" : "Error fetching data"};
+    } else {
+      response = {"error" : false,"message" : data};
+    }
+    res.json(response);
+  });
 });
+
+/**
+ * Función que permite añadir un usuario
+ */
+router.post('/', function(req,res) {
+  var db = new mongoOp();
+  var response = {};
+
+  db.tipoUser;
+  db.email = req.body.email;
+  db.pass = require('crypto')
+      .createHash('sha1')
+      .update(req.body.pass)
+      .digest('base64');
+  db.nombre = req.body.nombre;
+  db.name = req.body.apellidos;
+  db.fechaAlta = new Date();
+
+  db.save(function (err) {
+    if (err) {
+      response = {"error": true, "message": "Error adding data"};
+    } else {
+      response = {"error": false, "message": "Data added"};
+    }
+    res.json(response);
+  });
+});
+
+/**
+ * Función que devuelve el usuario correspondiente al 'id'
+ */
+router.get('/:id', function(req,res){
+  var response = {};
+  mongoOp.findById( req.params.id ,function(err,data){
+    if(err) {
+      response = {"error" : true,"message" : "Error fetching data"};
+    } else {
+      response = {"error" : false,"message" : data};
+    }
+    res.json(response);
+  });
+});
+
+/**
+ * Función que permite modificar el usuario correspondiente al 'id'.
+ */
+router.put('/:id', function(req,res){
+  var response = {};
+
+  mongoOp.findById(req.params.id,function(err,data){
+    if(err) {
+      response = {"error" : true,"message" : "Error fetching data"};
+    } else {
+      if(req.body.email !== undefined) {
+        data.email = req.body.email;
+      }
+      //Si se el mensaje lleva un nombre lo actualiza
+      if(req.body.nombre !== undefined) {
+        data.nombre = req.body.nombre;
+      }
+      //Si se el mensaje lleva una contraseña la actualiza
+      if(req.body.pass !== undefined) {
+        data.pass = require('crypto')
+            .createHash('sha1')
+            .update(req.body.pass)
+            .digest('base64');
+      }
+      //Si se el mensaje lleva un nombre lo actualiza
+      if(req.body.apellidos !== undefined) {
+        data.apellidos = req.body.apellidos;
+      }
+
+      data.save(function(err){
+        if(err) {
+          response = {"error" : true,"message" : "Error updating data"};
+        } else {
+          response = {"error" : false,"message" : "Data is updated for "+req.params.id};
+        }
+        res.json(response);
+      })
+    }
+  });
+})
+
+/**
+ * Función que permite eliminar el usuario que corresponde con el 'id'
+ */
+router.delete('/:id', function(req,res){
+  var response = {};
+
+  mongoOp.findById(req.params.id,function(err,data){
+    if(err) {
+      response = {"error" : true,"message" : "Error fetching data"};
+    } else {
+      mongoOp.remove({_id : req.params.id},function(err){
+        if(err) {
+          response = {"error" : true,"message" : "Error deleting data"};
+        } else {
+          response = {"error" : false,"message" : "Data associated with "+req.params.id+"is deleted"};
+        }
+        res.json(response);
+      });
+    }
+  });
+})
 
 module.exports = router;
