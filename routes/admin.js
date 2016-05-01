dfd7c1ddf59e55a20ef48147e0cd777e50a396da
diegozgz92 <diegosc_zgz92@hotmail.com>
2016-05-01 17:54:40 +0200

    Pagina Admin

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.sendfile('./views/pruebas.html');
+    res.sendfile('./views/admin.html');
 });
 
 module.exports = router;
