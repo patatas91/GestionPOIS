69dfe905eda96af37079deac4fded9c12c1a1e47
patatas91 <simon12_33@hotmail.com>
2016-05-04 20:35:35 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	bin/www

--- bin/views/userCore.js
+++ bin/views/userCore.js
@@ -6,11 +6,68 @@ angular.module('userManager',[],function($locationProvider){
 });
 
 var map;
+var markers = [];
+var markersUltimo = [];
+
+function setMarkers(lat, long) {
+    // Loop through markers and set map to null for each
+   for (var i=0; i<markers.length; i++) {
+        markers[i].setMap(null);
+   }
+
+    var marker = new google.maps.Marker({
+        position: {lat: lat, lng: long},
+        map: map,
+        icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+        animation: google.maps.Animation.DROP
+    });
+
+    markersUltimo.push(marker);
+}
+
+function focusPoi(lat, long, zoom) {
+    //map.setCenter(new google.maps.LatLng(lat, long));
+    map.panTo(new google.maps.LatLng(lat, long));
+    map.setZoom(zoom);
+}
+
+function resetfocus() {
+    map.panTo(new google.maps.LatLng(40.46366700000001, -3.7492200000000366));
+    map.setZoom(6);
+}
+
+function reloadMarkers() {
+    for(i=0;i<markersUltimo.length;i++) {
+        markersUltimo[i].setMap(null);
+    }
+
+    // Reset the markers array
+    markersUltimo = [];
+    markers = [];
+
+    $.get('/pois', function(res){
+        var message=res.message;
+        if(!res.error){
+            for(i=0;i<message.length;i++){
+                var marker = new google.maps.Marker({
+                    position: {lat: message[i].latitud, lng: message[i].longitud},
+                    map: map,
+                    animation: google.maps.Animation.DROP,
+                    title: message[i].nombre
+                });
+                // Push marker to markers array
+                markers.push(marker);
+            }
+        }
+    });
+}
 
 function mainController($scope, $http) {
     // when landing on the page, get all todos and show them
     $scope.map=true;
     $scope.showpoi=false;
+    $scope.showlista=true;
+    
 
     $http.get('/pois')
         .success(function(data) {
@@ -19,13 +76,17 @@ function mainController($scope, $http) {
         .error(function(data) {
             console.log('Error: ' + data);
         });    
-
+    
+    /* CAMBIA LISTA Y VISTA DE UN POI */
     $scope.viewPoi = function(id) {
-        if($scope.map){
-            $scope.map=false;
+        if($scope.showlista){
+            $scope.showlista=false;
             $http.get('/pois/'+id)
                 .success(function(data){
                     $scope.mypoi=data.message;
+                    //
+                    setMarkers(data.message.latitud, data.message.longitud);
+                    focusPoi(data.message.latitud, data.message.longitud, 18);
                 })
                 .error(function(data){
                     console.log('Error: '+ data);
@@ -33,13 +94,42 @@ function mainController($scope, $http) {
             $scope.showpoi=true;
         }
         else{
+            reloadMarkers();
+            resetfocus();
             $scope.showpoi=false;
-            $scope.map=true;
+            $scope.showlista=true;
         }
-    };  
+    };
 
-    $scope.viewMap = function() {
+    $scope.deleteMarkersFromMap = function() {
+        for (var i = 0; i<$scope.markers.length; i++) {
+            $scope.markers[i].setMap(null);
+        }
+    }
 
+    $scope.deleteMarkersFromArray = function() {
+        $scope.markers.length = 0;
+    }
+    
+    $scope.calculateDistance = function() {
+        var totalDistance = 0;
+        var partialDistance = [];
+        partialDistance.length = $scope.markers.length - 1;
+
+        for(var i = 0; i < partialDistance.length; i++){
+            var p1 = $scope.markers[i];
+            var p2 = $scope.markers[i+1];
+
+            var R = 6378137; // Earth’s mean radius in meter
+            var dLat = $scope.rad(p2.position.lat() - p1.position.lat());
+            var dLong = $scope.rad(p2.position.lng() - p1.position.lng());
+            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos($scope.rad(p1.position.lat())) * Math.cos($scope.rad(p2.position.lat())) *
+                Math.sin(dLong / 2) * Math.sin(dLong / 2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            totalDistance += R * c / 1000; //distance in Km
+            partialDistance[i] = R * c / 1000;
+        }
     }
 }
 
@@ -48,7 +138,7 @@ function initMap() {
         center: {lat: 40.46366700000001, lng: -3.7492200000000366},
         zoom: 6
     });
-    var markers=[];
+
     $.get('/pois', function(res){
         var message=res.message;
         if(!res.error){
@@ -56,11 +146,14 @@ function initMap() {
                 var marker = new google.maps.Marker({
                     position: {lat: message[i].latitud, lng: message[i].longitud},
                     map: map,
+                    animation: google.maps.Animation.DROP,
                     title: message[i].nombre
                 });
-                marker.setMap(map)
+                // Push marker to markers array
+                markers.push(marker);
             }
         }
     });
+    setMarkers(markers);
 }
 
